51ef3ec1a9f12e042246ded5ee06eb734420c110
Vo Anh Tu Nguyen <nguyenvoanhtu129@gmail.com>
2020-02-17 18:23:05 -0500

    Done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
-let circleTurn = false
+let circleTurn
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
 const WINNING_COMBINATIONS = [
@@ -10,30 +10,41 @@ const WINNING_COMBINATIONS = [
 ]
 const winningMessage = document.querySelector('[winning-message-text]')
 const winningMessageElement = document.getElementById('winningMessage')
+const btnRestart = document.getElementById('restartButton')
 
-
-setBoardHover()
-
-
-cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true })
-});
+startGame()
+btnRestart.addEventListener('click', startGame)
 
 
 function handleClick(e){
-
-    
-    // Place win
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
     if(checkWin(currentClass)){
         endGame(false)
-        console.log(winningMessageElement)
+        // console.log(winningMessageElement)
+    }else if(isDraw()){
+        endGame(true)
+    }else{
+        switchTurn()
+        setBoardHover()
     }
-    switchTurn()
-    setBoardHover()
+}
 
+
+function startGame(){
+    circleTurn = false
+    setBoardHover()
+    // cellElements.forEach(cell => {
+    //     cell.addEventListener('click', handleClick, { once: true })
+    // });
+    winningMessageElement.classList.remove('show')
+    cellElements.forEach(cell =>{
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click', handleClick)
+        cell.addEventListener('click', handleClick, { once: true })
+    })
 }
 
 
@@ -59,9 +70,16 @@ function checkWin(currentClass){
 
 function endGame(draw){
     if(draw){
-
+        winningMessage.innerText = 'Draw !!!'
     }else{
-        winningMessage.innerText = `${circleTurn ? "O's" : "X' s"} Wins !!!!`
+        winningMessage.innerText = `${circleTurn ? "O's" : "X's"} Wins !!!!`
     }
     winningMessageElement.classList.add('show')
+}
+
+function isDraw(){
+    //Destruct cellElements to make it become an array so that we can use every() method
+    return [...cellElements].every(cell=>{
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    })
 }
